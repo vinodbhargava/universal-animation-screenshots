ec53e90f62a1d6fd7625777e4e5c8022c83554c4
Vinod Bhargava <vinod.bhargava2005@gmail.com>
2016-06-29 11:27:22 +0530

    Initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: __filename,
+  selector: 'home',
+  styleUrls: [
+    'home.style.css'
+  ],
+  templateUrl: 'home.template.html'
+})
+export class Home {
+}
